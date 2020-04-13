f2b29a3cf16f7fbd37f808de62b9526531a54275
romenigld <romenigld@gmail.com>
2020-04-14 00:36:43 +0200

    Git Initialized and installed the Phoenix LiveView

---
+++ lib/my_app_web/router.ex
@@ -0,0 +1,28 @@
+defmodule MyAppWeb.Router do
+  use MyAppWeb, :router
+
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :put_root_layout, {MyAppWeb.LayoutView, :root}
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", MyAppWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", MyAppWeb do
+  #   pipe_through :api
+  # end
+end
